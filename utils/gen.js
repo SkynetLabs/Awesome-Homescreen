41874f3f8f9314f78c74a382b708e8a0df7227de
Matthew Sevey <mjsevey@gmail.com>
2021-09-27 13:56:22 -0400

    WIP

--- utils/gen.js
+++ utils/gen.js
@@ -5,8 +5,17 @@ const markdownTable = require('markdown-table');
 // Import json array
 const apps = require('../src/apps.json');
 
+// Build New Object
+let appsForMarkdown = [];
+apps.forEach(() => {
+    // Build fields
+    let jsonData = {}
+    jsonData['New Field'] = "testing";
+    appsForMarkdown.push(jsonData);
+})
+
 // Convert json to table
-const appsTabled = jsonToTable(apps);
+const appsTabled = jsonToTable(appsForMarkdown);
 
 // Convert table to markdown
 const appsMarkdown = markdownTable(appsTabled, { align: ['c', 'c', 'l'] })
