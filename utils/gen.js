bc1d795da124c08a33eba013406f946dca4f251f
Matthew Sevey <mjsevey@gmail.com>
2021-09-28 11:14:36 -0400

    Update apps.json layout and markdown generation

--- utils/gen.js
+++ utils/gen.js
@@ -7,10 +7,13 @@ const apps = require('../src/apps.json');
 
 // Build New Object
 let appsForMarkdown = [];
-apps.forEach(() => {
+let addToHomescreen = "[![Add to Homescreen](https://siasky.net/CADKZ7bTyVRjMmyMnEsUKDidqdmdaNHaJP25cp_3YGQlkg)](https://homescreen.hns.siasky.net/#/skylink/"
+apps.forEach((app) => {
     // Build fields
     let jsonData = {}
-    jsonData['New Field'] = "testing";
+    jsonData['Application'] = "[" + app.applicationName + "](" + app.repoLink + ")";
+    jsonData['Add To Homescreen'] = addToHomescreen + app.resolverSkylink + ")";
+    jsonData['Description'] = app.description;
     appsForMarkdown.push(jsonData);
 })
 
