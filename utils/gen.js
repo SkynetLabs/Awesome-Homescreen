a25e9d1d5fee1fbab0c901914d6bb302b8a33216
Daniel Helm <dan.helm@gmail.com>
2021-10-13 14:10:49 -0500

    change button to newest from docs

--- utils/gen.js
+++ utils/gen.js
@@ -8,34 +8,42 @@ const apps = require('../src/apps.json');
 // Build New Object
 let officialApps = [];
 let unofficialApps = [];
-let addToHomescreen = "[![Add to Homescreen](https://siasky.net/CADKZ7bTyVRjMmyMnEsUKDidqdmdaNHaJP25cp_3YGQlkg)](https://homescreen.hns.siasky.net/#/skylink/"
+let addToHomescreen =
+  '[![Add to Homescreen](https://img.shields.io/badge/Skynet-Add%20To%20Homescreen-00c65e?logo=skynet&labelColor=0d0d0d)](https://homescreen.hns.siasky.net/#/skylink/';
 apps.forEach((app) => {
-    // Build fields
-    let jsonData = {}
-    jsonData['Application'] = "[" + app.applicationName + "](" + app.repoLink + ")";
-    jsonData['Add To Homescreen'] = addToHomescreen + app.resolverSkylink + ")";
-    jsonData['Description'] = app.description;
-    if (app.unofficial) {
-        unofficialApps.push(jsonData);
-    } else {
-        officialApps.push(jsonData);
-    }
-})
+  // Build fields
+  let jsonData = {};
+  jsonData['Application'] =
+    '[' + app.applicationName + '](' + app.repoLink + ')';
+  jsonData['Add To Homescreen'] = addToHomescreen + app.resolverSkylink + ')';
+  jsonData['Description'] = app.description;
+  if (app.unofficial) {
+    unofficialApps.push(jsonData);
+  } else {
+    officialApps.push(jsonData);
+  }
+});
 
 // Convert json to table
 const officialAppsTabled = jsonToTable(officialApps);
 const unofficialAppsTabled = jsonToTable(unofficialApps);
 
 // Convert table to markdown
-const officialAppsMarkdown = markdownTable(officialAppsTabled, { align: ['c', 'c', 'l'] })
-const unofficialAppsMarkdown = markdownTable(unofficialAppsTabled, { align: ['c', 'c', 'l'] })
+const officialAppsMarkdown = markdownTable(officialAppsTabled, {
+  align: ['c', 'c', 'l'],
+});
+const unofficialAppsMarkdown = markdownTable(unofficialAppsTabled, {
+  align: ['c', 'c', 'l'],
+});
 
 // Log the official apps
 console.log(officialAppsMarkdown);
 
 // Log the Header for the Unofficial Apps sections
-console.log("### Unofficial Apps");
-console.log("Below is a list of unofficial apps that can be used as examples of what can be supported on Homescreen. These should not be considered production apps.");
+console.log('### Unofficial Apps');
+console.log(
+  'Below is a list of unofficial apps that can be used as examples of what can be supported on Homescreen. These should not be considered production apps.'
+);
 
 // Log the unofficial apps
 console.log(unofficialAppsMarkdown);
